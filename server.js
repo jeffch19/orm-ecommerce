c7a91ae9f26d550ac5936d167327c1d365cbc5f5
jeffch19 <17jcummings@gmail.com>
2023-12-22 15:50:22 -0500

    added to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// server.js
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const { sequelize } = require('./config/connection'); // Import sequelize instance
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +9,22 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Sync Sequelize models to the MySQL database on server start
+const syncDatabase = async () => {
+  try {
+    await sequelize.sync({ force: false }); // Set force to true to drop and recreate tables
+
+    console.log('Sequelize models synchronized with the database');
+  } catch (error) {
+    console.error('Error synchronizing Sequelize models with the database:', error);
+  }
+};
+
+// Call the syncDatabase function before including routes
+syncDatabase();
+
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
